bcf40eb0119279e7375d5d3230e8b129a6a6c295
Duval <duval.celine@hotmail.com>
2020-02-07 13:18:06 +0100

    ancien html

---
+++ CLIENT/ancienne page/javascript.js
@@ -0,0 +1,16 @@
+var i =2;
+console.log(i);
+
+function showK(){ document.getElementById('divchoixK').style.visibility = 'visible';
+                 document.getElementById('divchoixdecoupebase').style.visibility = 'hidden';
+
+}
+
+function showDecoupageBase(){ document.getElementById('divchoixdecoupebase').style.visibility = 'visible';         document.getElementById('divchoixK').style.visibility = 'hidden';
+}
+
+
+
+
+
+
