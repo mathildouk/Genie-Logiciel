696df83e9b1edbad3d1eed43ef0239fb72beec59
Duval <duval.celine@hotmail.com>
2020-03-12 16:23:27 +0100

    tri git

---
+++ MML-classification-master/org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/MmlParsingJavaTest.java
@@ -0,0 +1,238 @@
+package org.xtext.example.mydsl.tests;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.extensions.InjectionExtension;
+import org.eclipse.xtext.testing.util.ParseHelper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.xtext.example.mydsl.generator.MMLCompiler;
+
+import org.xtext.example.mydsl.mml.MMLModel;
+
+
+import com.google.inject.Inject;
+
+@ExtendWith(InjectionExtension.class)
+@InjectWith(MmlInjectorProvider.class)
+public class MmlParsingJavaTest {
+
+	@Inject
+	ParseHelper<MMLModel> parseHelper;
+	
+	@Test
+	public void loadModel() throws Exception {
+		MMLModel result = parseHelper.parse("datainput \"foo.csv\"\n"
+				+ "mlframework scikit-learn\n"
+				+ "algorithm DT\n"
+				+ "mlframework R\n"
+				+"algorithm DT\n"
+				+ "TrainingTest { percentageTraining 70 }\n"
+				+ "F1\n"
+				+ "");
+		Assertions.assertNotNull(result);
+		EList<Resource.Diagnostic> errors = result.eResource().getErrors();
+		Assertions.assertTrue(errors.isEmpty(), "Unexpected errors");			
+		Assertions.assertEquals("foo.csv", result.getInput().getFilelocation());			
+		
+	}		
+	
+	@Test
+	public void compileDataInput() throws Exception {
+		//un exemple particulier conforme  à la grammaire Xtext-MMLModel -> il faudrait le généraliser
+		MMLModel result = parseHelper.parse("datainput \"/home/id1019/Documents/iris.csv\"\n" + 
+				"mlframework R\n" + 
+				"algorithm DT 20\n" + 
+				"mlframework R\n" + 
+				"algorithm RF\n" + 
+				"mlframework R\n" + 
+				"algorithm LogisticRegression\n" + 
+				"mlframework R\n" + 
+				"algorithm XGboost\n" + 
+				"mlframework R\n" + 
+				"algorithm SVM gamma= 8 C=2 kernel= radial classification nu-classification\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm DT 20\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm RF\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm LogisticRegression\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm XGboost\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm SVM gamma= 8 C=2 kernel= radial classification nu-classification\n" + 
+				"TrainingTest { percentageTraining 70 }\n" + 
+				"balanced_accuracy recall precision accuracy F1 macro_recall macro_precision macro_accuracy macro_F1"
+				);
+		
+		
+		
+		MMLCompiler mmlcompiler = new MMLCompiler(result);
+		mmlcompiler.compute();
+		
+		
+	}
+
+	@Test
+	public void compileRTrainTest() throws Exception {
+		//un exemple particulier conforme  à la grammaire Xtext-MMLModel -> il faudrait le généraliser
+		MMLModel result = parseHelper.parse("datainput \"/home/id1019/Documents/iris.csv\"\n" + 
+				"mlframework R\n" + 
+				"algorithm DT 20\n" + 
+				"mlframework R\n" + 
+				"algorithm RF\n" + 
+				"mlframework R\n" + 
+				"algorithm LogisticRegression\n" + 
+				"mlframework R\n" + 
+				"algorithm XGboost\n" + 
+				"mlframework R\n" + 
+				"algorithm SVM gamma= 8 C=2 kernel= radial classification nu-classification\n" + 
+				"mlframework R\n" + 
+				"algorithm SVM gamma= 8 classification c-classification\n" + 
+				"TrainingTest { percentageTraining 70 }\n" + 
+				"balanced_accuracy recall precision accuracy F1 macro_recall macro_precision macro_accuracy macro_F1"
+				);
+		
+		
+		
+		MMLCompiler mmlcompiler = new MMLCompiler(result);
+		mmlcompiler.compute();
+		
+		
+	}
+	@Test
+	public void compileRSVM() throws Exception {
+		//un exemple particulier conforme  à la grammaire Xtext-MMLModel -> il faudrait le généraliser
+		MMLModel result = parseHelper.parse("datainput \"/home/id1019/Documents/iris.csv\"\n" + 			
+				"mlframework R\n" + 
+				"algorithm SVM gamma= 8 C=40 kernel= radial classification nu-classification\n" + 
+				"mlframework R\n" + 
+				"algorithm SVM gamma= 8 classification one-classification\n" + 
+				"TrainingTest { percentageTraining 70 }\n" + 
+				"balanced_accuracy recall precision accuracy F1 macro_recall macro_precision macro_accuracy macro_F1"
+				);
+		
+		
+		
+		MMLCompiler mmlcompiler = new MMLCompiler(result);
+		mmlcompiler.compute();
+		
+		
+	}
+	@Test
+	public void compilePYTHONSVM() throws Exception {
+		//un exemple particulier conforme  à la grammaire Xtext-MMLModel -> il faudrait le généraliser
+		MMLModel result = parseHelper.parse("datainput \"/home/id1019/Documents/iris.csv\"\n" + 			
+				"mlframework scikit-learn\n" + 
+				"algorithm SVM gamma= 8 C=40 kernel= radial classification nu-classification\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm SVM gamma= 8 classification one-classification\n" + 
+				"TrainingTest { percentageTraining 70 }\n" + 
+				"balanced_accuracy recall precision accuracy F1 macro_recall macro_precision macro_accuracy macro_F1"
+				);
+		
+		
+		
+		MMLCompiler mmlcompiler = new MMLCompiler(result);
+		mmlcompiler.compute();
+		
+		
+	}
+	
+	@Test
+	public void compileRCV() throws Exception {
+		//un exemple particulier conforme  à la grammaire Xtext-MMLModel -> il faudrait le généraliser
+		MMLModel result = parseHelper.parse("datainput \"/home/id1019/Documents/iris.csv\"\n" + 
+				"mlframework R\n" + 
+				"algorithm DT 20\n" + 
+				"mlframework R\n" + 
+				"algorithm RF\n" + 
+				"mlframework R\n" + 
+				"algorithm LogisticRegression\n" + 
+				"mlframework R\n" + 
+				"algorithm XGboost\n" + 
+				"mlframework R\n" + 
+				"algorithm SVM gamma= 8 C=2 kernel= radial classification nu-classification\n" + 
+				"mlframework R\n" + 
+				"algorithm SVM gamma= 8 classification c-classification\n" + 
+				"CrossValidation { numRepetitionCross 10 }\n" + 
+				"balanced_accuracy recall precision accuracy F1 macro_recall macro_precision macro_accuracy macro_F1"
+				);
+		
+		
+		
+		MMLCompiler mmlcompiler = new MMLCompiler(result);
+		mmlcompiler.compute();
+		
+		
+	}
+	
+	@Test
+	public void compilePYTHONTrainTest() throws Exception {
+		//un exemple particulier conforme  à la grammaire Xtext-MMLModel -> il faudrait le généraliser
+		MMLModel result = parseHelper.parse("datainput \"/home/id1019/Documents/iris.csv\"\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm DT 20\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm RF\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm LogisticRegression\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm XGboost\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm SVM gamma= 8 C=2 kernel= radial classification nu-classification\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm SVM gamma= 8 classification c-classification\n" + 
+				"TrainingTest { percentageTraining 70 }\n" + 
+				"balanced_accuracy recall precision accuracy F1 macro_recall macro_precision macro_accuracy macro_F1"
+				);
+		
+		
+		
+		MMLCompiler mmlcompiler = new MMLCompiler(result);
+		mmlcompiler.compute();
+		
+		
+	}
+	
+	@Test
+	public void compilePYTHONCV() throws Exception {
+		//un exemple particulier conforme  à la grammaire Xtext-MMLModel -> il faudrait le généraliser
+		MMLModel result = parseHelper.parse("datainput \"/home/id1019/Documents/iris.csv\"\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm DT 20\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm RF\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm LogisticRegression\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm XGboost\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm SVM gamma= 8 C=2 kernel= radial classification nu-classification\n" + 
+				"mlframework scikit-learn\n" + 
+				"algorithm SVM gamma= 8 classification c-classification\n" + 
+				"CrossValidation { numRepetitionCross 10 }\n" + 
+				"balanced_accuracy recall precision accuracy F1 macro_recall macro_precision macro_accuracy macro_F1"
+				);
+		
+		
+		
+		MMLCompiler mmlcompiler = new MMLCompiler(result);
+		mmlcompiler.compute();
+		
+		
+	}
+		
+
+
+	
+	
+	
+
+}
---
+++ mml-webapp/src/main/java/Main.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.python.google.common.io.Files;
+import org.xtext.example.mydsl.generator.MMLCompiler;
+import org.xtext.example.mydsl.mml.MMLModel;
+
+import io.javalin.Javalin;
+import io.javalin.core.util.FileUtil;
+
+public class Main {
+
+  
+    public static void main(String[] args) {
+
+  
+		
+        Javalin app = Javalin.create(config -> {
+            config.addStaticFiles("/public");
+            
+        }).start(8080);
+
+        app.post("/generate", ctx -> {
+        	//If the user chooses the mml editor
+        	Files.write(ctx.formParam("mml").getBytes(), new File("scripts_upload/code.mml")); 
+        	processMML(ctx.formParam("mml"));
+        	
+        	//generate the html results visualisation
+			TableGenerator table_generator = new TableGenerator();
+			Files.write(table_generator.getHtml().getBytes(), new File("target/classes/public/visuResults.html")); 
+			
+        	
+        	ctx.redirect("/results-choose.html");
+        });
+
+        app.post("/mml-form-generate", ctx -> {
+        	//If the user chooses the form
+        	
+        	ctx.uploadedFiles("myFile").forEach(file -> {
+                FileUtil.streamToFile(file.getContent(), "scripts_upload/upload/" + file.getFilename());
+            });
+        	
+        	FormParser form_parser = new FormParser(ctx);
+        	Files.write(form_parser.mml.getBytes(), new File("scripts_upload/code.mml")); 
+        	processMML(form_parser.mml);
+        	
+    		
+			//generate the html results visualisation
+			TableGenerator table_generator = new TableGenerator();
+			Files.write(table_generator.getHtml().getBytes(), new File("target/classes/public/visuResults.html")); 
+			
+				
+        	ctx.redirect("/results-choose.html");
+        });
+        
+        
+       
+    }
+
+	private static void processMML(String mmlContent) throws IOException {
+		//call the compiler needed
+		MMLLoader mmlLoader = new MMLLoader();	    
+		MMLModel mml = mmlLoader.loadModel(mmlContent);
+		
+		MMLCompiler mmlcompiler = new MMLCompiler(mml);
+		mmlcompiler.compute();
+		
+	}
+	
+	
+
+}
+
+
+
+
